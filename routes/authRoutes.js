65461e24eeaa426aeee293c95b484cfdef95a81d
Dmitriy <dmitriy@Dmitriys-Mac-mini.local>
2020-05-13 13:49:43 +0300

    added clarification about login action

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,6 +6,7 @@ const router = Router();
 
 router.post('/login', (req, res, next) => {
     try {
+        // TODO: Implement login action (get the user if it exist with entered credentials)
         res.data = data;
     } catch (err) {
         res.err = err;
