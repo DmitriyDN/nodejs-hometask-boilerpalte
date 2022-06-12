dd7b901d3c21c9d82ec6867e0965603196ca2aaa
VladyslavLohynskyi <watcher.good2003@gmail.com>
2022-06-12 23:31:57 +0300

    added routers and services for fighters

--- routes/fighterRoutes.js
+++ routes/fighterRoutes.js
@@ -1,9 +1,18 @@
-const { Router } = require('express');
-const FighterService = require('../services/fighterService');
-const { responseMiddleware } = require('../middlewares/response.middleware');
-const { createFighterValid, updateFighterValid } = require('../middlewares/fighter.validation.middleware');
+const { Router } = require("express");
+const FighterService = require("../services/fighterService");
+const { responseMiddleware } = require("../middlewares/response.middleware");
+const {
+  createFighterValid,
+  updateFighterValid,
+} = require("../middlewares/fighter.validation.middleware");
 
 const router = Router();
+
+router.get("/", FighterService.getFighters);
+router.get("/:id", FighterService.getFighter);
+router.post("/", FighterService.postFighter);
+router.put("/:id", FighterService.updateFighter);
+router.delete("/:id", FighterService.deleteFighter);
 
 // TODO: Implement route controllers for fighter
 
--- services/fighterService.js
+++ services/fighterService.js
@@ -1,7 +1,64 @@
-const { FighterRepository } = require('../repositories/fighterRepository');
+const { FighterRepository } = require("../repositories/fighterRepository");
 
 class FighterService {
-    // TODO: Implement methods to work with fighters
+  // TODO: Implement methods to work with fighters
+  getFighters(req, res) {
+    const fighters = FighterRepository.getAll();
+    return res.json(fighters);
+  }
+  getFighter(req, res) {
+    const { id } = req.params;
+    const fighter = FighterRepository.getOne({ id });
+    if (!fighter) {
+      return res.json({ message: "User with id " + id + " not exist" });
+    }
+    return res.json(fighter);
+  }
+
+  postFighter(req, res) {
+    const { name, health, power, defense } = req.body;
+    const findByName = FighterRepository.getOne({ name });
+
+    if (findByName) {
+      return res.json({ message: "Fighter with this name is exist" });
+    }
+
+    const newFighter = FighterRepository.create({
+      name,
+      health,
+      power,
+      defense,
+    });
+
+    return res.json(newFighter);
+  }
+
+  updateFighter(req, res) {
+    const { id } = req.params;
+    const data = req.body;
+    const fighter = FighterRepository.getOne({ id });
+    if (!fighter) {
+      return res.json({ message: "Fighter with this id not exist" });
+    }
+    const updatedFighter = FighterRepository.update(id, data);
+    return res.json({
+      message: "Data is successfully updated",
+      updatedFighter,
+    });
+  }
+
+  deleteFighter(req, res) {
+    const { id } = req.params;
+    const fighter = FighterRepository.getOne({ id });
+    if (!fighter) {
+      return res.json({ message: "Fighter with this id not exist" });
+    }
+    const deletedFighter = FighterRepository.delete(id);
+    return res.json({
+      message: "Data is successfully deleted",
+      deletedFighter,
+    });
+  }
 }
 
 module.exports = new FighterService();
