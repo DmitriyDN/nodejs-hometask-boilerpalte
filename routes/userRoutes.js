6b725926ef85f294946c127f6d4ef7039dcd4e7f
VladyslavLohynskyi <watcher.good2003@gmail.com>
2022-06-12 19:05:28 +0300

    added routes and services for user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,18 @@
-const { Router } = require('express');
-const UserService = require('../services/userService');
-const { createUserValid, updateUserValid } = require('../middlewares/user.validation.middleware');
-const { responseMiddleware } = require('../middlewares/response.middleware');
+const { Router } = require("express");
+const UserService = require("../services/userService");
+const {
+  createUserValid,
+  updateUserValid,
+} = require("../middlewares/user.validation.middleware");
+const { responseMiddleware } = require("../middlewares/response.middleware");
 
 const router = Router();
+
+router.get("/", UserService.getUsers);
+router.get("/:id", UserService.getUser);
+router.post("/", UserService.postUser);
+router.put("/:id", UserService.updateUser);
+router.delete("/:id", UserService.deleteUser);
 
 // TODO: Implement route controllers for user
 
--- services/userService.js
+++ services/userService.js
@@ -1,16 +1,62 @@
-const { UserRepository } = require('../repositories/userRepository');
+const { UserRepository } = require("../repositories/userRepository");
 
 class UserService {
+  getUsers(req, res) {
+    const users = UserRepository.getAll();
+    return res.json(users);
+  }
 
-    // TODO: Implement methods to work with user
-
-    search(search) {
-        const item = UserRepository.getOne(search);
-        if(!item) {
-            return null;
-        }
-        return item;
+  getUser(req, res) {
+    const { id } = req.params;
+    const user = UserRepository.getOne({ id });
+    if (!user) {
+      return res.json({ message: "User with id " + id + " not exist" });
     }
+    return res.json(user);
+  }
+
+  postUser(req, res) {
+    const { firstname, lastname, email, phoneNumber, password } = req.body;
+    const findByEmail = UserRepository.getOne({ email });
+    const findByNumber = UserRepository.getOne({ phoneNumber });
+    if (findByNumber) {
+      return res.json({ message: "User with this phone number is not exist" });
+    }
+    if (findByEmail) {
+      return res.json({ message: "User with this email is not exist" });
+    }
+
+    const newUser = UserRepository.create({
+      firstname,
+      lastname,
+      email,
+      phoneNumber,
+      password,
+    });
+
+    return res.json(newUser);
+  }
+
+  updateUser(req, res) {
+    const { id } = req.params;
+    const data = req.body;
+    const user = UserRepository.getOne({ id });
+    if (!user) {
+      return res.json({ message: "User with this id not exist" });
+    }
+    const updatedUser = UserRepository.update(id, data);
+    return res.json({ message: "Data is successfully updated", updatedUser });
+  }
+
+  deleteUser(req, res) {
+    const { id } = req.params;
+    const user = UserRepository.getOne({ id });
+    if (!user) {
+      return res.json({ message: "User with this id not exist" });
+    }
+    const deletedUser = UserRepository.delete(id);
+    return res.json({ message: "Data is successfully deleted", deletedUser });
+  }
 }
 
 module.exports = new UserService();
