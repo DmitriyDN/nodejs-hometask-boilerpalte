e5a9336b48a81527cd4662871f48b23dc8226c6b
VladyslavLohynskyi <watcher.good2003@gmail.com>
2022-06-13 03:28:33 +0300

    added auth for users

--- services/authService.js
+++ services/authService.js
@@ -1,13 +1,14 @@
-const UserService = require('./userService');
+const { UserRepository } = require("../repositories/userRepository");
 
 class AuthService {
-    login(userData) {
-        const user = UserService.search(userData);
-        if(!user) {
-            throw Error('User not found');
-        }
-        return user;
+  login(req, res) {
+    const { email, password } = req.body;
+    const user = UserRepository.getOne({ email, password });
+    if (!user) {
+      return res.json({ message: "User not found" });
     }
+    return res.json(user);
+  }
 }
 
 module.exports = new AuthService();
